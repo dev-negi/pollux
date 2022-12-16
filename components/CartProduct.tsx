9cccc9e054b0d9d79e9f836e78e76353f62b23db
Devendra Negi <devendera.negi@gmail.com>
2022-12-16 17:57:45 +0530

    Add to cart and cart page

---
+++ components/CartProduct.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
+
+import { urlForThubnail } from "../utils";
+interface Props {
+  items: Product[];
+  id: string;
+}
+
+function CartProduct({ id, items }: Props) {
+  const removeItemFromBasket = () => {};
+  return (
+    <div className="flex flex-col gap-x-4 border-b border-gray-300 pb-5 lg:flex-row lg:items-center">
+      <div className="relative h-44 w-44">
+        <Image
+          src={urlForThubnail(items[0].image[0]).url()}
+          alt={items[0].title}
+          width={300}
+          height={300}
+          layout="responsive"
+        ></Image>
+      </div>
+      <div className="flex flex-1 items-end lg:items-center">
+        <div className="flex-1 space-y-4">
+          <div className="flex flex-col gap-x-8 text-xl lg:flex-row lg:text-2xl">
+            <h4 className="font-semibold lg:w-96">{items[0].title}</h4>
+            <p className="flex items-end gap-x-1 font-semibold">
+              {items.length}
+              <ChevronDownIcon className="h-6 w-6 text-blue-500" />
+            </p>
+          </div>
+          <p className="flex cursor-pointer items-end text-blue-500 hover:underline">
+            Show product details
+            <ChevronDownIcon className="h-6 w-6" />
+          </p>
+        </div>
+        <div className="flex flex-col items-end space-y-4">
+          <h4 className="text-xl font-semibold lg:text-2xl">
+            <div>
+              {items.reduce((total, item) => total + item.price, 0)}
+              USD
+            </div>
+          </h4>
+          <button
+            onClick={removeItemFromBasket}
+            className="text-blue-500 hover:underline"
+          >
+            Remove
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CartProduct;
---
+++ pages/cart.tsx
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { XCircleIcon } from "@heroicons/react/outline";
+
+import Layout from "../components/Layout";
+import CartProduct from "../components/CartProduct";
+import Button from "../rango/Button";
+import { selectBasketItems } from "../redux/basketSlice";
+
+function Cart() {
+  const items = useSelector(selectBasketItems);
+  const router = useRouter();
+  const [groupedItemsInBasket, setGroupedItemsInBasket] = useState(
+    {} as { [key: string]: Product[] }
+  );
+
+  useEffect(() => {
+    const groupedItems = items.reduce((results, item) => {
+      (results[item._id] = results[item._id] || []).push(item);
+      return results;
+    }, {} as { [key: string]: Product[] });
+
+    setGroupedItemsInBasket(groupedItems);
+  }, [items]);
+
+  return (
+    <Layout title="Shopping Cart">
+      <div className="mx-auto max-w-5xl pb-24">
+        <div className="px-5">
+          <h1 className="my-4 text-3xl font-semibold lg:text-4xl">
+            {items.length > 0 ? "Review your bag." : "Your bag is empty."}
+          </h1>
+          {items.length === 0 && (
+            <Button
+              title="Continue Shopping"
+              width="w-50"
+              height="h-10"
+              color="bg-indigo-600"
+              hoverColor="bg-indigo-500"
+              onClick={() => router.push("/")}
+            />
+          )}
+        </div>
+        {items.length >= 0 && (
+          <div className="mx-5 md:mx-8">
+            {Object.entries(groupedItemsInBasket).map(([key, items]) => (
+              <CartProduct key={key} items={items} id={key} />
+            ))}
+            <div className="my-12 mt-6 ml-auto max-w-3xl">
+              <div className="divide-y divide-gray-300">
+                <div className="pb-4">
+                  <div className="flex justify-between">
+                    <p>Subtotal</p>
+                    <p>
+                      {/* <Currency quantity={basketTotal} currency="USD" /> */}
+                    </p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p>Shipping</p>
+                    <p>FREE</p>
+                  </div>
+                  <div className="flex justify-between">
+                    <div className="flex flex-col gap-x-1 lg:flex-row"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </Layout>
+  );
+}
+
+export default Cart;

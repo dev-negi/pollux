64c143116bf7a4afa5feda52889a245feb4bfee5
Devendra Negi <devendera.negi@gmail.com>
2022-12-20 12:10:59 +0530

    mobile-header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,73 +1,24 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useSelector } from "react-redux";
-import {
-  ShoppingBagIcon,
-  MagnifyingGlassIcon,
-  UserCircleIcon,
-  Bars3Icon,
-} from "@heroicons/react/24/outline";
 
 import { brandName } from "../constant";
-
 import { selectBasketItems } from "../redux/basketSlice";
+import DesktopHeader from "./desktop/DesktopHeader";
+import MobileHeader from "./mobile/MobileHeader";
+
 function Header() {
   const items = useSelector(selectBasketItems);
-  const [offset, setOffset] = useState(0);
-  // TODO Clean code for transistion
-  const mainDiv =
-    offset > 350
-      ? "flex fixed pl-10 pr-10 pt-3 pb-3 w-screen z-50 bg-white transition ease-in duration-500 shadow-lg"
-      : "flex p-10 relative h-auto transition ease-out duration-500 opacity-1";
-  useEffect(() => {
-    const onScroll = () => setOffset(window.pageYOffset);
-    // clean up code
-    window.removeEventListener("scroll", onScroll);
-    window.addEventListener("scroll", onScroll, { passive: true });
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
-
-  useEffect(() => {}, []);
 
   return (
-    <header className="relative h-auto">
-      <div className={mainDiv}>
-        <div className="flex flex-1 items-center flex-wrap justify-between mr-auto">
-          <div className="">
-            <nav className="">
-              <ul className="flex justify-between hover:bottom-1 ">
-                <li className="header-menu-link">Home</li>
-                <li className="header-menu-link">Infent</li>
-                <li className="header-menu-link">Boys</li>
-                <li className="header-menu-link">Girls</li>
-                <li className="header-menu-link">Party Wear</li>
-                <li className="header-menu-link">Ethinc</li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-        <div className="justify-cente text-3xl font-bold">{brandName}</div>
-        <div className="flex flex-1 justify-end gap-x-5">
-          <div className="flex justify-center items-center">
-            <MagnifyingGlassIcon className="w-6 h-6" />
-            <div className="pl-4 text-xs">Search</div>
-          </div>
-          <div className="flex justify-center items-center">
-            <UserCircleIcon className="w-6 h-6" />
-            <div className="pl-4 text-xs">
-              <Link href="/login">Login</Link>
-            </div>
-          </div>
-          <div className="flex first-letter:justify-center items-center">
-            <span className="relative text-[12px] left-11 top-3 font-bold bg-yellow-200 rounded-full pr-2 pl-2 pt-1 pb-1">
-              {items.length}
-            </span>
-            <ShoppingBagIcon className="w-8 h-8" />
-            <div className="pl-4 text-xs"></div>
-          </div>
-        </div>
+    <>
+      <div className="md:hidden">
+        <MobileHeader itemInCart={items.length} brandName={brandName} />
       </div>
-    </header>
+      <div className="hidden md:block">
+        <DesktopHeader itemInCart={items.length} brandName={brandName} />
+      </div>
+    </>
   );
 }
 

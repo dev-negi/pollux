5c903b267e2abbb02e1151330f973d617f01c0bd
Devendra Negi <devendera.negi@gmail.com>
2022-12-20 10:21:53 +0530

    Header removed code

--- components/Header.tsx
+++ components/Header.tsx
@@ -54,11 +54,13 @@ function Header() {
           </div>
           <div className="flex justify-center items-center">
             <UserCircleIcon className="w-6 h-6" />
-            <div className="pl-4 text-xs">Account</div>
+            <div className="pl-4 text-xs">
+              <Link href="/login">Login</Link>
+            </div>
           </div>
           <div className="flex first-letter:justify-center items-center">
             <span className="relative text-[12px] left-11 top-3 font-bold bg-yellow-200 rounded-full pr-2 pl-2 pt-1 pb-1">
-              0
+              {items.length}
             </span>
             <ShoppingBagIcon className="w-8 h-8" />
             <div className="pl-4 text-xs"></div>
@@ -70,55 +72,3 @@ function Header() {
 }
 
 export default Header;
-
-{
-  /* <nav className="flex h-20 items-center px-4 justify-between shadow-md">
-        <div className="text-lg font-bold">
-          <Link href="/">{brandName}</Link>
-        </div>
-        <div className="flex">
-          <div className="p-2">
-            <Link href="/cart">
-              {items?.length > 0 && (
-                <span className="absolute right-0 top-0 z-50 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-[10px] text-white">
-                  {items.length}
-                </span>
-              )}
-              Cart
-            </Link>
-          </div>
-          <div className="p-2">
-            <Link href="/login">Login</Link>
-          </div>
-        </div>
-      </nav> */
-}
-
-{
-  /* <header className="h-auto border-2 border-red-500">
-<div className="p-10">
-  <div className="flex items-center flex-wrap justify-between mr-auto">
-    <div className="">
-      <nav className="">
-        <ul className="flex justify-between items-center">
-          <li className="relative mr-10">Home</li>
-          <li className="relative mr-10">Infent</li>
-          <li className="relative mr-10">Boys</li>
-          <li className="relative mr-10">Girls</li>
-          <li className="relative mr-10">Party Wear</li>
-          <li className="relative mr-10">Ethinc</li>
-        </ul>
-      </nav>
-    </div>
-    <div className="flex-1 flex justify-center text-3xl font-bold">
-      {brandName}
-    </div>
-    <div className="flex justify-end items-center ml-auto">
-      <div className="ml-10">search</div>
-      <div className="ml-10">account</div>
-      <div className="ml-10">cart Count</div>
-    </div>
-  </div>
-</div>
-</header> */
-}
